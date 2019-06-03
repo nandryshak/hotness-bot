b7e855a142db91188996b181256c496cb713a5d5
Nick Andryshak <nandryshak@gmail.com>
2019-06-03 12:48:20 -0400

    Sets and removes hot icons

--- index.js
+++ index.js
@@ -6,14 +6,27 @@ const client = new Discord.Client();
 const COMMANDS = {
     // hotlink:
     // hoticon:
-    // nohot:
+    // nohot: ,
     hot: maybeUpdateHotter,
 };
 
+const hotnessSettings = {
+    enabled: false,
+    icon: '🔥',
+
+    words: 5,
+    byUsers: 1,
+    inMinutes: 2,
+    coolAfterMinutes: 1,
+
+    excludedChannels: [],
+};
+
+const coolingTimeouts = {};
+
 function maybeUpdateHotter(message) {
     // W words, by X users, in Y minutes, remove icon after Z minutes
-    const args = message.content.replace('.hot', '').trim().split(' ').map(parseFloat).filter(arg => arg === NaN);
-    console.log("args:", args);
+    const args = message.content.replace('.hot', '').trim().split(' ').map(parseFloat).filter(arg => arg != NaN);
 
     if (args.length === 0) {
         return toggleHotter();
@@ -25,19 +38,57 @@ function maybeUpdateHotter(message) {
 }
 
 function toggleHotter() {
-    return 'toggleHotter'
+    hotnessSettings.enabled = !hotnessSettings.enabled;
+    return `hotter is now ${hotnessSettings.enabled ? 'enabled' : 'disabled'}!`;
 }
 
 function updateHotter(args) {
-    return 'updateHotter'
+    const hs = hotnessSettings;
+    hs.enabled = true;
+    hs.words = args[0];
+    hs.byUsers = args[1];
+    hs.inMinutes = args[2];
+    hs.coolAfterMinutes = args[3];
+
+    return `hotness is now enabled! settings are: ${hs.words} words, by ${hs.byUsers} users, in ${hs.inMinutes} minutes. Remove icon after ${hs.coolAfterMinutes} minutes.`;
 }
 
 function help() {
-    return 'help'
+    return 'help';
+}
+
+function timestampFromSnowflake(id) {
+    return new Date((id / 4194304) + 1420070400000);
 }
 
 function checkHotness(message) {
-    console.log("checking hotness");
+    const cutoffTime = new Date(new Date() - hotnessSettings.inMinutes * 60000);
+    const latestMessages = message.channel.messages.filter(msg => {
+        const date = timestampFromSnowflake(msg.id);
+        return date >= cutoffTime;
+    });
+    const numberOfWords = latestMessages.map(msg => msg.content).join(' ').split(' ').length;
+    const numberOfUsers = new Set(latestMessages.map(msg => msg.author.id)).size;
+    if (numberOfWords >= hotnessSettings.words && numberOfUsers >= hotnessSettings.byUsers) {
+        setChannelHot(message);
+    }
+}
+
+function setChannelHot(message) {
+    const channel = message.channel;
+    const icon = hotnessSettings.icon;
+    channel.setName(icon + channel.name + icon);
+
+    if (coolingTimeouts[channel.id]) {
+        clearTimeout(coolingTimeouts[channel.id]);
+    }
+
+    coolingTimeouts[channel.id] = setTimeout(() => {
+        channel.setName(channel.name.replace(icon, '').replace(icon, ''));
+        channel.send(`This channel has cooled off.`);
+    }, hotnessSettings.coolAfterMinutes * 60 * 1000);
+
+    channel.send(hotnessSettings.icon + `This channel is HOT` + hotnessSettings.icon);
 }
 
 function dispatchCommand(message) {
@@ -46,7 +97,10 @@ function dispatchCommand(message) {
             return message.reply(COMMANDS[cmd](message));
         }
     }
-    return checkHotness(message);
+
+    if (message.author.id !== client.user.id) {
+        return checkHotness(message);
+    }
 }
 
 client.on('ready', () => console.log(`Logged in as ${client.user.tag}!`));
