73327b1821b70f8e372e8a6451168831d6042bd3
Nick Andryshak <nandryshak@gmail.com>
2019-06-03 10:39:56 -0400

    Simple command dispatching

--- index.js
+++ index.js
@@ -3,14 +3,52 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+const COMMANDS = {
+    // hotlink:
+    // hoticon:
+    // nohot:
+    hot: maybeUpdateHotter,
+};
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
+function maybeUpdateHotter(message) {
+    // W words, by X users, in Y minutes, remove icon after Z minutes
+    const args = message.content.replace('.hot', '').trim().split(' ').map(parseFloat).filter(arg => arg === NaN);
+    console.log("args:", args);
+
+    if (args.length === 0) {
+        return toggleHotter();
+    } else if (args.length === 4) {
+        return updateHotter(args);
+    } else {
+        return help();
     }
-});
+}
 
+function toggleHotter() {
+    return 'toggleHotter'
+}
+
+function updateHotter(args) {
+    return 'updateHotter'
+}
+
+function help() {
+    return 'help'
+}
+
+function checkHotness(message) {
+    console.log("checking hotness");
+}
+
+function dispatchCommand(message) {
+    for (const cmd in COMMANDS) {
+        if (message.content.startsWith('.' + cmd)) {
+            return message.reply(COMMANDS[cmd](message));
+        }
+    }
+    return checkHotness(message);
+}
+
+client.on('ready', () => console.log(`Logged in as ${client.user.tag}!`));
+client.on('message', dispatchCommand);
 client.login(process.env.DISCORD_TOKEN);
