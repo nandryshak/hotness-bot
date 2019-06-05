93d979bfdadf5996fe9da5c99bc1e04ad141fbd8
Nick Andryshak <nandryshak@gmail.com>
2019-06-05 11:57:58 -0400

    Better whitelist, hotChannels

--- index.js
+++ index.js
@@ -21,11 +21,15 @@ const hotnessSettings = {
     inMinutes: 3,
     coolAfterMinutes: 15,
 
-    whitelist: [],
-    hotChannels: {},
+    whitelist: new Set(),
+    hotChannels: [],
 };
 
 const coolingTimeouts = {};
+
+function saveSettings() {
+
+}
 
 function parseArgs(message) {
     return message.content.trim().split(' ').slice(1);
@@ -65,24 +69,27 @@ function updateHotter(args) {
     hs.byUsers = args[1];
     hs.inMinutes = args[2];
     hs.coolAfterMinutes = args[3];
-
+    saveSettings();
     return `hotness is now enabled! settings are: ${hs.words} words, by ${hs.byUsers} users, in ${hs.inMinutes} minutes. Remove icon after ${hs.coolAfterMinutes} minutes.`;
 }
 
 function hotenablehere(message) {
     const args = parseArgs(message);
     if (noArgs(args)) {
-        if (!hotnessSettings.whitelist.find(c => c.id == message.channel.id)) {
-            hotnessSettings.whitelist.push(message.channel);
-            return `hotness is now enabled for this channel!`;
-        }
+        hotnessSettings.whitelist.add(message.channel.id);
+        saveSettings();
+        return `hotness is now enabled for this channel!`;
     } else {
         return help();
     }
 }
 
-function hotsettings() {
-    return '```' + JSON.stringify(hotnessSettings, undefined, 4) + '```';
+function hotsettings(message) {
+    const settingsCopy = Object.assign({}, hotnessSettings);
+    settingsCopy.whitelist = Array.from(settingsCopy.whitelist).map(cid => message.guild.channels.find(c => c.id === cid).name)
+    settingsCopy.hotChannels = Array.from(settingsCopy.hotChannels).map(c => c.hotName);
+    const settingsJSON = JSON.stringify(settingsCopy, undefined, 4);
+    return '```' + settingsJSON + '```';
 }
 
 function help() {
@@ -101,14 +108,14 @@ function checkHotness(message) {
     });
     const numberOfWords = latestMessages.map(msg => msg.content).join(' ').split(' ').length;
     const numberOfUsers = new Set(latestMessages.map(msg => msg.author.id)).size;
-    const channelInWhitelist = hotnessSettings.whitelist.find(c => c.id == message.channel.id);
+    const channelInWhitelist = hotnessSettings.whitelist.has(message.channel.id);
     if (numberOfWords >= hotnessSettings.words && numberOfUsers >= hotnessSettings.byUsers && channelInWhitelist) {
         setChannelHot(message);
     }
 }
 
 function channelIsHot(channel) {
-    return !!hotnessSettings.hotChannels[channel.id];
+    return !!hotnessSettings.hotChannels.find(c => c.id == channel.id);
 }
 
 function setChannelHot(message) {
@@ -117,9 +124,13 @@ function setChannelHot(message) {
 
     // Set name and send a message only if channel is not already hot.
     if (!channelIsHot(channel)) {
-        channel.setName(icon + channel.name + icon);
+        const oldName = channel.name;
+        const hotName = icon + oldName + icon;
+        channel.setName(hotName);
         channel.send(hotnessSettings.icon + `This channel is HOT` + hotnessSettings.icon);
-        hotnessSettings.hotChannels[channel.id] = channel;
+        const hotChannel = { id: channel.id, hotName: hotName, oldName: oldName };
+        console.log("hotChannel:", hotChannel);
+        hotnessSettings.hotChannels.push(hotChannel);
     }
 
     // Set cooling timeout to remove icons after channel cools off.
@@ -127,8 +138,13 @@ function setChannelHot(message) {
         clearTimeout(coolingTimeouts[channel.id]);
     }
     coolingTimeouts[channel.id] = setTimeout(() => {
-        channel.setName(channel.name.replace(icon, '').replace(icon, ''));
-        hotnessSettings.hotChannels[channel.id] = undefined;
+        const hotChannel = hotnessSettings.hotChannels.find(c => c.id === channel.id);
+        console.log("cooling hotChannel:", hotChannel);
+        const oldName = hotChannel.oldName;
+        if (oldName) {
+            channel.setName(oldName);
+            hotnessSettings.hotChannels = hotnessSettings.hotChannels.filter(c => c.id !== channel.id);
+        }
     }, hotnessSettings.coolAfterMinutes * 60 * 1000);
 
 }
